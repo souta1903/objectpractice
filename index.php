746cac45ab3ba999c1c4c6e6212c28e682b378bf
souta1903 <93124330+souta1903@users.noreply.github.com>
2021-11-14 12:00:28 +0900

    クラスを定義

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
-<?php require_once('data.php') ?>
+<?php
+    class Product{
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Antique Furniture</title>
-    <link rel="stylesheet" href="./style.css">
-</head>
-<body>
-<div class="lineup">
-    <h1 class="title">Antique Furniture</h1>
+    }
 
-        <div class="product-names">
-        <?php foreach ($products as $product): ?>
-          <div class="prodcut-item">
-            <img src="<?php echo $product->getImage() ?>" class="prodcut-item-image">
-            <h3 class="product-item-name"><?php echo $product->getName() ?></h3>
-            <p class="price">¥<?php echo $product->getTaxIncludedPrice() ?>（税込）</p>
-        </div>
-        <?php endforeach ?>
+$table= new Product();
+$chair= new Product();
 
-</div>
-    
-</body>
-
-</html>
+?>
