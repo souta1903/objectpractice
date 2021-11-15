062c02cc9e7537dbc14598c07306471a23ea6b87
souta1903 <93124330+souta1903@users.noreply.github.com>
2021-11-16 08:11:15 +0900

    インスタンスを編集

--- data.php
+++ data.php
@@ -6,6 +6,5 @@ $chair = new Product('CHAIR',3000,'asset/chair.jpeg');
 $light = new Product('LIGHT',4000,'asset/light.jpeg');
 $bed = new Product('BED',10000,'asset/sofa.jpeg');
 
-$products=array($table,$chair,$light,$bed);
-// $products = [$table,$chair,$light,$bed];
+$products = [$table,$chair,$light,$bed];
 ?>
