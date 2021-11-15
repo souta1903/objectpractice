18be9c0cb66946bed0989ba14cce6948ce3beecd
souta1903 <93124330+souta1903@users.noreply.github.com>
2021-11-15 13:23:23 +0900

    クラスを定義

--- lineup.php
+++ lineup.php
@@ -1,35 +1,29 @@
-<?php 
-class Product{
-    private $name;
-    private $price;
-    private $image;
-    private $orderCount= 0;
+<?php
+    class Product{
+      private $name;
+      private $price;
+      private $image;
+      private $ordercount = 0;
 
-    public function __construct($name, $price, $image) {
-        $this->name = $name;
-        $this->price = $price;
-        $this->image = $image;
-      }
-
-    public function getName() {
+      public function __construct($name,$price,$image){
+            $this->name=$name;
+            $this->price=$price;
+            $this->image=$image;
+          }
+      public function getName(){
         return $this->name;
       }
-      
-    public function getImage() {
+      public function getImage(){
         return $this->image;
       }
-      
-    public function getOrderCount() {
-        return $this->orderCount;
+      public function gettaxinprice(){
+        return floor($this->price *1.08);
       }
-      
-    public function setOrderCount($orderCount) {
-        $this->orderCount = $orderCount;
+      public function getordercount(){
+        return $this->ordercount;
       }
-      
-    public function getTaxIncludedPrice() {
-        return floor($this->price * 1.08);
+      public function getTotalprice(){
+        return $this->gettaxinprice()*$this->ordercount;
       }
-      
-}
+    }
 ?>
