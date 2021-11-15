7b0487bc4bffe1bdd9e7bf79866eb152495c916e
souta1903 <93124330+souta1903@users.noreply.github.com>
2021-11-15 13:23:01 +0900

    商品情報を入力

--- data.php
+++ data.php
@@ -1,17 +1,11 @@
-<?php
+<?php 
 require_once('lineup.php');
 
-$chair= new Product('Chair',3000,'https://pixabay.com/images/id-2155376/');
-$sofa=  new Product('sofa',10000,'light.jpeg');
-$light= new Product('light',5000,'sofa.jpeg'); 
-$table= new Product('table',7000,'table.jpeg');
+$table=new Product('TABLE',5000,'tabel.jpeg');
+$chair=new Product('CHAIR',3000,'chair.jpeg');
+$light=new Product('LIGHT',4000,'light.jpeg');
+$bed=new Product('BED',10000,'sofa.jpeg');
 
-
-
-
-$products =array($chair,$sofa,$light,$table);
-
-
-
+$products=array($table,$chair,$light,$bed);
 
 ?>
