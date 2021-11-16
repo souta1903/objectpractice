a664f4b5550d2750bb5164dea8727abbd031f56b
souta1903 <93124330+souta1903@users.noreply.github.com>
2021-11-16 11:19:28 +0900

    プロダクトを変更

--- data.php
+++ data.php
@@ -4,7 +4,7 @@ require_once('lineup.php');
 $table = new Product('TABLE',5000,'asset/table.jpeg');
 $chair = new Product('CHAIR',3000,'asset/chair.jpeg');
 $light = new Product('LIGHT',4000,'asset/light.jpeg');
-$bed = new Product('BED',10000,'asset/sofa.jpeg');
+$SOFA = new Product('SOFA',10000,'asset/sofa.jpeg');
 
-$products = [$table,$chair,$light,$bed];
+$products = [$table,$chair,$light,$SOFA];
 ?>
